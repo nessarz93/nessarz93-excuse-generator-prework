016e4e1d2d10f6f1d1b55efd8065f3ffc907bdd2
Vanessa <126210620+nessarz93@users.noreply.github.com>
2023-07-25 00:22:15 +0000

    Terminado

--- src/app.js
+++ src/app.js
@@ -2,22 +2,27 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  
-  let who = ['The dog', 'My grandma', 'His turtle', 'My bird'];
-  let action = ['ate', 'peed', 'crushed', 'broke'];
-  let what = ['my homework', 'the keys', 'the car'];
-  let when = ['before the class', 'right on time', 'when I finished', 'during my lunch', 'while I was praying'];
-  
-  for(let i=0; i < who.length; i++) {
-      for(let j=0; j < action.length; j++) {
-          for(let k=0; k < what.length, k++){
-            for(let l=0; l < when.length, l++){
-              console.log(who[i] + action[j] + what[k] + when[l]);
-          }
-      }
-    }
-  };
+  let quien = who[Math.floor(Math.random() * 4)];
+  let accion = action[Math.floor(Math.random() * 4)];
+  let que = what[Math.floor(Math.random() * 4)];
+  let donde = when[Math.floor(Math.random() * 4)];
+
+  let result = quien + " " + accion + " " + que + " " + donde;
+
+  let excuseElement = document.getElementById("result");
+  excuseElement.textContent = result;
+
+  console.log(result);
+};
+
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
