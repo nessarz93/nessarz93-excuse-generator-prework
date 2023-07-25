fe01430773fbd6734f76e869d6e00d49d9dfbe35
Vanessa <126210620+nessarz93@users.noreply.github.com>
2023-07-25 00:13:50 +0000

    Javascript completo

--- src/app.js
+++ src/app.js
@@ -6,6 +6,18 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  
+  let who = ['The dog', 'My grandma', 'His turtle', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'the keys', 'the car'];
+  let when = ['before the class', 'right on time', 'when I finished', 'during my lunch', 'while I was praying'];
+  
+  for(let i=0; i < who.length; i++) {
+      for(let j=0; j < action.length; j++) {
+          for(let k=0; k < what.length, k++){
+            for(let l=0; l < when.length, l++){
+              console.log(who[i] + action[j] + what[k] + when[l]);
+          }
+      }
+    }
+  };
